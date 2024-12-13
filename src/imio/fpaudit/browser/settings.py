a2620c406b615f81546f11048f685accbfb6de72
Stephan Geulette <s.geulette@imio.be>
2024-12-13 16:25:14 +0100

    Added plone6 buildout files

--- src/imio/fpaudit/browser/settings.py
+++ src/imio/fpaudit/browser/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from collective.z3cform.datagridfield import DataGridFieldFactory
+from collective.z3cform.datagridfield.datagridfield import DataGridFieldFactory
 from collective.z3cform.datagridfield.registry import DictRow
 from imio.fpaudit import _
 from imio.fpaudit.storage import store_config
